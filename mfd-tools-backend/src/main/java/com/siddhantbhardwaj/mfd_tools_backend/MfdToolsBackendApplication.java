29f9a7e58dec1851fa7f6218009da2ee13d6fa7c
Siddhant Bhardwaj <bhardwajsiddhant03@gmail.com>
2025-12-10 16:31:03 +0530

    first commit

---
+++ mfd-tools-backend/src/main/java/com/siddhantbhardwaj/mfd_tools_backend/MfdToolsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siddhantbhardwaj.mfd_tools_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfdToolsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfdToolsBackendApplication.class, args);
+	}
+
+}
---
+++ mfd-tools-backend/src/test/java/com/siddhantbhardwaj/mfd_tools_backend/MfdToolsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhantbhardwaj.mfd_tools_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfdToolsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
